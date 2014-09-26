39d22fa27aaff070db51e3f315caf73249a97ed7
unknown <amokeeper@gmail.com>
2014-09-26 10:42:26 +0800

    newtest app commit

---
+++ newtest/list.py
@@ -0,0 +1,10 @@
+﻿
+from django.shortcuts import render_to_response
+
+address = [
+    {'name':'zhansan ', 'address':'我靠'},
+    {'name':'死定了你', 'address':'我靠'}
+    ]
+
+def index(request):
+    return render_to_response('list.html', {'address': address})
